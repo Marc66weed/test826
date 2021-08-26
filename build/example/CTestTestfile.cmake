7ec177c11ede5358ad356ea2f6bffbb5c4971bbb
marc <marc66weed@gmail.com>
2021-08-26 17:58:20 +0800

    Lesson Learning add workspace add package add hello_world.cpp

---
+++ build/example/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/marc/test826/src/example
+# Build directory: /home/marc/test826/build/example
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
